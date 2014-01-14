a98420d43fc8de2251693f98e6562543370b9f0a
Erik Osheim <d_m@plastic-idolatry.com>
2014-01-14 13:42:22 -0500

    Fix glaring problems, fix memoization, add features.
    After waking up I realized that I hadn't really tested things.
    This commit should make the stream actually usable in practice.
    
    One thing that didn't totally work last night was memoization.
    I wanted to be able to support the "stock scala" stream example:
    
      val fibs: Stream[Int] =
        0 #:: 1 #:: (fibs zip fibs.tail).map(t => t._1 + t._2)
    
    In Scala, memoization means this is moderately efficient (but
    also breaks streams for many other purposes). Even though
    PspStream is not memoized by default I still wanted to be able
    to support this. But due to the recursive definition, a simple
    call to .memoize() would not work.
    
    I ended up changing the Memo class' name to ~::, and adding
    a ~:: method, which is like #:: but memoizes too. This means
    the stock scala example (with memoization) becomes:
    
      val fibs: PspStream[Int] =
        0 ~:: 1 ~:: (fibs zip fibs.tail).map(t => t._1 + t._2)
    
    As was noted before, the existing of more than two ADT nodes
    for PspStream makes matching against it less attractive
    (it exposes the underlying design). But I think it's a worthy
    trade-off in this case.

--- core/src/main/scala/PspStream.scala
+++ core/src/main/scala/PspStream.scala
@@ -16,43 +16,43 @@ package linear
  */
 
 object PspStream {
-  def empty[A] = nil.castTo[PspStream[A]]
+  def empty[A] = snil.castTo[PspStream[A]]
   def apply[A](as: A*): PspStream[A] = as.foldRight(snil[A])(_ #:: _)
   def unfold[A](a: A)(f: A => A): PspStream[A] = a #:: unfold(f(a))(f)
-  def from(n: Long): PspStream[Long] = unfold(n)(_ + 1L)
+  def from(n: Int): PspStream[Int] = unfold(n)(_ + 1)
 
   implicit def pspStreamOps[A](rhs: => PspStream[A]): PspStreamOps[A] =
     new PspStreamOps(rhs _)
+
 }
 
 class PspStreamOps[A](val rhs: () => PspStream[A]) extends AnyVal {
   def #::(lhs: A): PspStream[A] = new #::(lhs, rhs)
   def #:::(lhs: PspStream[A]): PspStream[A] = lhs append rhs()
+
+  def ~::(lhs: A): PspStream[A] = new ~::(lhs, rhs)
 }
 
-sealed trait PspStream[A] extends AnyRef with PspLinear[A] {
+sealed trait PspStream[A] extends AnyRef with PspLinear[A] { lhs =>
   def isEmpty: Boolean
   def head: A
   def tail: PspStream[A]
 
   def memoize: PspStream[A] = this match {
-    case x #:: xsf => Memo(x, xsf)
+    case x #:: xsf => new ~::(x, xsf)
     case st => st
   }
 
   def fold[B](b: B)(f: (A, () => PspStream[A]) => B): B = this match {
     case x #:: xsf => f(x, xsf)
-    case Memo(x, xsf) => f(x, xsf)
+    case x ~:: xsf => f(x, xsf)
     case _ => b
   }
 
   private def upcast[A1 >: A] : PspStream[A1] = this.castTo[PspStream[A1]]
 
   def take(n: Int): PspStream[A] =
-    if (n < 1 || isEmpty) snil() else head #:: tail.take(n - 1)
-
-  def takeWhile(f: A => Boolean): PspStream[A] =
-    if (isEmpty || !f(head)) snil[A] else head #:: tail.takeWhile(f)
+    if (n < 1 || isEmpty) snil[A] else head #:: tail.take(n - 1)
 
   def drop(n: Int): PspStream[A] = {
     def loop(n: Int, st: PspStream[A]): PspStream[A] =
@@ -60,14 +60,33 @@ sealed trait PspStream[A] extends AnyRef with PspLinear[A] {
     loop(n, this)
   }
 
+  def zip[B](rhs: PspStream[B]): PspStream[(A, B)] =
+    if (lhs.isEmpty || rhs.isEmpty) snil[(A, B)]
+    else (lhs.head, rhs.head) #:: (lhs.tail zip rhs.tail)
+
+  def takeWhile(f: A => Boolean): PspStream[A] =
+    if (isEmpty || !f(head)) snil[A] else head #:: tail.takeWhile(f)
+
+  def interleave(xs: PspStream[A]): PspStream[A] =
+    if (isEmpty) xs else head #:: (xs interleave tail)
+
   def append(rhs: => PspStream[A]): PspStream[A] =
     if (isEmpty) rhs else head #:: (tail append rhs)
+
+  def appendAll(rhs: PspLinear[PspStream[A]]): PspStream[A] =
+    if (rhs.isEmpty) lhs else lhs append rhs.head.appendAll(rhs.tail)
 
   final def foreach(f: A => Unit): Unit = {
     @tailrec def loop(p: PspStream[A]): Unit =
       if (!p.isEmpty) { f(p.head); loop(p.tail) } else ()
     loop(this)
   }
+
+  final def map[B](f: A => B): PspStream[B] =
+    if (isEmpty) snil[B] else f(head) #:: tail.map(f)
+
+  final def flatMap[B](f: A => PspStream[B]): PspStream[B] =
+    if (isEmpty) snil[B] else f(head).append(tail.flatMap(f))
 
   final override def toString =
     if (isEmpty) "PspStream()" else s"PspStream($head, ...)"
@@ -89,8 +108,8 @@ final case class #::[A](head: A, tailf: () => PspStream[A]) extends PspStream[A]
   def tail = tailf()
 }
 
-final case class Memo[A](head: A, tailf: () => PspStream[A]) extends PspStream[A] {
+final case class ~::[A](head: A, tailf: () => PspStream[A]) extends PspStream[A] {
   def sizeInfo = precise(1).atLeast
   def isEmpty = false
-  lazy val tail: PspStream[A] = tailf().memoize
+  lazy val tail = tailf().memoize
 }
